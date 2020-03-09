abca3c9da0524e750841d7737ff04dc8c9cebfa3
NickMcGettigan <nick.mcgettigan@gmail.com>
2020-03-08 22:32:19 -0700

    Login Page, Link to Signup Page, force user authentication, Add logout buttons.  Correct Test DB lookup to display returned information.

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth import authenticate, login, logout # handle authentication (email)
 
 
 # Create your views here.
@@ -8,6 +9,12 @@ def signup_view(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            # Authentication via email, on hold for now
+            # need to include authenticate backends if reimplementing
+            # username = form.cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password')
+            # user = authenticate(username=username, password=raw_password)
+            # login(request, user)
             #log the user in
             return redirect('homepage')
 
