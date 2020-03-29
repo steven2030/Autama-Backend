8f5ef7bbac34536c10c0f5e0b5dc5ef81e4c3365
MdoubleA <needanothermichael@gmail.com>
2020-03-28 21:20:54 -0700

    Clean code. Remove bad comments and views implemented as classes.

--- AutamaProfiles/views.py
+++ AutamaProfiles/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
 from django.forms import ModelForm
 from AutamaProfiles.models import AutamaProfile
-from accounts.views import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
-from django.views import View
-import json
 
 
-# Create your views here.
 class AutamaForm(ModelForm):
     class Meta:
         model = AutamaProfile
@@ -21,9 +16,6 @@ def register_autama(request):
         form = AutamaForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-#            autama_instance = form.save(commit=False)
-#            autama_instance.slug = str(autama_instance.pk) + autama_instance.first + autama_instance.last
-            # redirect to another page
             return redirect('about')
 
     else:
@@ -41,21 +33,3 @@ def browse(request):
     profiles = AutamaProfile.objects.all()
     return render(request, 'AutamaProfiles/browse.html', {'profiles': profiles})
 
-
-class RobotView(LoginRequiredMixin, View):
-    def get(self, request):
-        robot_id = request.POST.get("robot_id")
-        if robot_id:
-            robot = AutamaProfile.objects.get(id=int(robot_id))
-        else:
-            robot = AutamaProfile.objects.all()[0]
-        return render(request, '../templates/robot.html', {"robot": robot})
-
-    def post(self, request):
-        robot_id = request.POST.get("robot_id")
-        obj = AutamaProfile.objects.get(id=int(robot_id))
-        obj.match_number += 1
-        obj.creator = request.user.username
-        obj.save()
-        return HttpResponse(json.dumps({'code': 0, "creator": obj.creator, "match_number": obj.match_number}),
-                            content_type="application/json")
