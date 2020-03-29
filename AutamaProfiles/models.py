08866076539c06be1d85d2242ea1be68ef43709e
MdoubleA <needanothermichael@gmail.com>
2020-03-28 21:24:25 -0700

    Clean code. Remove unused imports and bas comments.

--- AutamaProfiles/models.py
+++ AutamaProfiles/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-from django.db.models.signals import pre_save
 from django.utils.text import slugify
 from django.shortcuts import reverse
-
-# Create your models here.
 
 
 class AutamaProfile(models.Model):
