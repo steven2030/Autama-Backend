c811eb9829d0e70332ba70e447bb7b5706341a3e
MdoubleA <needanothermichael@gmail.com>
2020-03-28 21:36:32 -0700

    Remove unused urls and views. Clean up Autama browse.html, make look a little nicer with basic html.

--- AutamaProfiles/urls.py
+++ AutamaProfiles/urls.py
@@ -7,6 +7,5 @@ app_name = 'AutamaProfiles'
 urlpatterns = [
     path('', AutamaProfile_views.browse, name='browse'),
     path('register/', AutamaProfile_views.register_autama, name='register_autama'),
-    path('robot/', AutamaProfile_views.RobotView.as_view(), name='robot'),
     path('<int:pk>-<slug:slug>/', AutamaProfile_views.profile, name='profile'),
 ]
