7e144862f92b1f51a0d78687af0de399fc73384a
timbercoder <jacklai@pdx.edu>
2020-03-30 11:41:30 -0700

    Add name checking when conversing

--- Nucleus/ham.py
+++ Nucleus/ham.py
@@ -9,8 +9,9 @@ from Nucleus.tools import read_json
 
 
 class Ham:
-    def __init__(self, autama_id: str):
+    def __init__(self, name: str, autama_id: str):
         self.__nucleus = Nucleus()
+        self.__name = name
         self.__file_name = autama_id + ".json"
         self.__personality = read_json(self.__file_name)
 
@@ -18,6 +19,15 @@ class Ham:
     def display_personality(self):
         self.__nucleus.display_personality(self.__personality)
 
-    # Method returns Autama's output by taking in user's input
+    # A method returns Autama's output by taking in user's input
     def converse(self, user_input: str):
         return self.__nucleus.converse_with(self.__personality, user_input)
+
+    # Method checks for name and then converse
+    def name_check_converse(self, user_input: str):
+        if "what is your name" in user_input or "name" in user_input:
+            return "my name is " + self.__name
+        elif "who are you" in user_input or "who dis" in user_input:
+            return "i'm " + self.__name
+        else:
+            return self.converse(user_input)
