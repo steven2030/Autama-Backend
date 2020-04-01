8ea3655cc55ec336461f9fd38635647da0f83f64
Hardy7 <33678638+Hardy7@users.noreply.github.com>
2020-03-31 21:45:42 -0700

    Update extratags.py

--- accounts/templatetags/extratags.py
+++ accounts/templatetags/extratags.py
@@ -1,19 +1,14 @@
 # -*- coding:utf-8 -*-
-__version__ = '1.0.0.0'
-"""
-@brief  :   简介
-@details:   详细信息
-@author :   zhphuang
-@date   :   2020-03-04
-"""
+__version__ = '3.0.3'
+
 
 from django import template
 
 register = template.Library()
 
 
-@register.filter(name='format_none')  # 过滤器在模板中使用时的name
-def format_none(value):  # 把传递过来的参数arg替换为'~'
+@register.filter(name='format_none')  
+def format_none(value): 
     if not value:
         return ''
     return value
