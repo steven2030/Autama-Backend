d0c1faa8cb3cccb46af618af88c0b1aec3d91e93
timbercoder <jacklai@pdx.edu>
2020-04-03 16:05:13 -0700

    Added citation

--- Nucleus/pancake.py
+++ Nucleus/pancake.py
@@ -1,7 +1,13 @@
 """
 PANCAKE = Preparing Autama's Name Carefully And Kinda Equally
 
-This file contains a class to handle generating random name.
+This file contains a class to handle generating random name. The class
+reads in names from two files, and then randomly picks one to
+generate as the name. One of the files is called female_names.txt,
+and it contains the top 1000 female names from 2018. The other file
+is called male_names.txt, and it contains the top 1000 male names
+from 2018. The names are from Social Security' site which could be
+found here: https://www.ssa.gov/cgi-bin/popularnames.cgi
 """
 
 from random import choice, seed
