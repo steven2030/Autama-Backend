a414c7524bea592840df46c7eacf4f3def357dd7
MdoubleA <needanothermichael@gmail.com>
2020-04-06 22:09:55 -0700

    Align autama ModelForm with new model.

--- AutamaProfiles/views.py
+++ AutamaProfiles/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required
 class AutamaForm(ModelForm):
     class Meta:
         model = AutamaProfile
-        fields = ['creator', 'picture', 'first', 'last']  # interests did exist here.
+        fields = ['creator', 'picture', 'first', 'last', 'interest1', 'interest2', 'interest3', 'interest4', 'interest5', 'interest6']  # interests did exist here.
 
 
 @login_required
