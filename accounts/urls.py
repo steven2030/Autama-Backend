2e23d34a18a162bf9a9d75be9c9b5b951abb01fe
haojian <haojian@pdx.edu>
2020-04-07 00:43:42 -0700

    Hao: add reset password class and change picture class in the accounts, make navbar to responsive in the base html, and change the userProfile style in the profile html.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views as accounts_views
+from accounts.views import  *
 
 
 app_name = 'accounts'  # For something akin to namespacing.
@@ -7,5 +8,7 @@ app_name = 'accounts'  # For something akin to namespacing.
 urlpatterns = [
     path('register/', accounts_views.RegisterView.as_view(), name="register"),
     #path('profile/edit', accounts_views.edit_profile, name='edit_user_profile'),
-    path('profile/', accounts_views.ProfileView.as_view(), name='profile')
+    path('profile/', accounts_views.ProfileView.as_view(), name='profile'),
+    path('change-avatar/', ChangeAvatarView.as_view(), name='change-avatar'),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
 ]
