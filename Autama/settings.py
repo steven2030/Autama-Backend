ef19d58daa71a6e6bfeb2ab53a851de7db0fcee7
Ubuntu <ubuntu@ip-172-31-17-221.us-west-2.compute.internal>
2020-04-13 05:53:27 +0000

    Update static location and settings to run both API and serve static pages.

--- Autama/settings.py
+++ Autama/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = '0jx6nply=59sv85ii0*r9abdg$sf+awn1vjwelfzc7pxfaek-3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['73.164.205.208']  ##
 ALLOWED_HOSTS = ['34.221.163.101', '127.0.0.1']
-
 
 # Application definition
 INSTALLED_APPS = [
@@ -38,14 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Autama',
-    'api',  # 'api.apps.ApiConfig',
+    'api', 
     'rest_framework',
     'rest_framework.authtoken',
-    ##'corsheaders'   # #
 ]
 
 
-# Dev Only, need to configure for production
+# Involved with EMAIL authentication of accounts 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
@@ -57,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    ##'corsheaders.middleware.CorsMiddleware'  # #
 ]
 
 ROOT_URLCONF = 'Autama.urls'
@@ -79,6 +75,7 @@ TEMPLATES = [
 ]
 
 # WSGI - web server gateway interface - only serves static pages
+# Not being used
 WSGI_APPLICATION = 'Autama.wsgi.application'
 
 # ASGI - Asynchronous Server Gateway Interface
@@ -132,26 +129,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-# TODO: Check path
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # <- absolute path to dir to collect static files for deployment
-# '/var/www/example.com/static/'
-# STATIC_URL = '/static/'  # <- has to end in a / if set to non-empty value
-STATIC_ROOT = os.path.join(BASE_DIR, "Webapp/")  # idk if Webapp or static for these
-STATIC_URL = '/Webapp/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
+# directories that are moved to the /static/ folder when 
+# python manage.py collectstatic is run.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Images'),
+#     os.path.join(BASE_DIR, 'Images'), # Included below under MEDIA
+    os.path.join(BASE_DIR, 'Webapp'),
 ]
 
 # Media
 MEDIA_URL = '/Images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Images')
-
-# TODO: Do we need this?
-# URL REDIRECTS
-# LOGIN_URL = '/login/'
-# LOGIN_REDIRECT_URL = '/FindMatches/'
-# LOGOUT_REDIRECT_URL = '/login/'
 
 # Authentication
 # AUTHENTICATION_BACKENDS = ('user_profile.views.CustomBackend',)
@@ -169,6 +159,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-##CORS_ORIGIN_ALLOW_ALL = True  # #
 
 
