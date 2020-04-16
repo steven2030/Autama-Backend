82b2517dc5345c04e362c638521b59b025c44da0
timbercoder <jacklai@pdx.edu>
2020-04-15 22:41:01 -0700

    Does not work. Commented out to fix later.

--- Nucleus/bacon.py
+++ Nucleus/bacon.py
@@ -1,7 +1,7 @@
 """
 BACON = Building Autama's Core Overall Nature
 
-This file contains a class to handle generating and saving an Autama's personality.
+This file contains a class to handle generating an Autama's personality.
 """
 
 from Nucleus.nucleus import Nucleus
@@ -12,8 +12,8 @@ class Bacon:
         self.__extension = ".json"
         self.__nucleus = Nucleus()
 
-    def generate_identity(self, autama_id: str):
+    """def generate_identity(self, autama_id: str):
         personality = self.__nucleus.generate_personality()
         self.__nucleus.display_personality(personality)
         file = autama_id + self.__extension
-        return personality
+        return personality"""
