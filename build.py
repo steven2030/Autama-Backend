45876ab1552236e37f280ffee2e160c45c07e0c7
timbercoder <jacklai@pdx.edu>
2020-04-15 23:20:08 -0700

    This creates nucleus.pickle

---
+++ build.py
@@ -0,0 +1,12 @@
+"""This file contains code to build a Nucleus. Run this file by entering
+"python build.py" to generate a nucleus.pickle file."""
+
+from Nucleus.tools import build_nucleus
+
+
+def main():
+    build_nucleus()
+
+
+if __name__ == '__main__':
+    main()
