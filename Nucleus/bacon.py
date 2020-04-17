0b43aa4ecbfc38afb265eb079fda501b968b36aa
timbercoder <jacklai@pdx.edu>
2020-04-17 15:02:45 -0700

    updated it to work with the restructured nucleus

--- Nucleus/bacon.py
+++ Nucleus/bacon.py
@@ -4,16 +4,33 @@ BACON = Building Autama's Core Overall Nature
 This file contains a class to handle generating an Autama's personality.
 """
 
-from Nucleus.nucleus import Nucleus
+from itertools import chain
+from random import choice
+from Nucleus.utils import get_dataset
+from Nucleus.tools import read_pickle
 
 
 class Bacon:
     def __init__(self):
-        self.__extension = ".json"
-        self.__nucleus = Nucleus()
+        self.__nucleus = read_pickle("nucleus.pickle")
+        self.__args = self.__nucleus.get_args()
+        self.__tokenizer = self.__nucleus.get_tokenizer()
 
-    """def generate_identity(self, autama_id: str):
-        personality = self.__nucleus.generate_personality()
-        self.__nucleus.display_personality(personality)
-        file = autama_id + self.__extension
-        return personality"""
+    # A method to generate a random personality. It returns a list of personality trait strings.
+    def generate_personality(self):
+        encoded_personality = self.__generate_encoded_personality()
+        return self.__decode_personality(encoded_personality)
+
+    # A method for generating one encoded personality
+    def __generate_encoded_personality(self):
+        dataset = get_dataset(self.__tokenizer, self.__args.dataset_path, self.__args.dataset_cache)
+        personalities = [dialog["personality"] for dataset in dataset.values() for dialog in dataset]
+        personality = choice(personalities)
+        return personality
+
+    # A method for decoding an encoded personality
+    def __decode_personality(self, encoded_personality: list):
+        encoded_personality_string = self.__tokenizer.decode(chain(*encoded_personality))
+        decoded_personality_list = [i + "." for i in encoded_personality_string.split(".")]
+        decoded_personality_list.pop()
+        return decoded_personality_list
