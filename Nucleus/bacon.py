1044c7fd6db2a142e86b9297d36748c3644a2be4
timbercoder <jacklai@pdx.edu>
2020-04-22 14:02:32 -0700

    add method to generate required amount of personality traits

--- Nucleus/bacon.py
+++ Nucleus/bacon.py
@@ -15,9 +15,19 @@ class Bacon:
         self.__nucleus = read_pickle("nucleus.pickle")
         self.__args = self.__nucleus.get_args()
         self.__tokenizer = self.__nucleus.get_tokenizer()
+        self.__required_amount = 6 # The required amount of traits per personality
+
+    # A method to generate a random personality with exactly the required amount of traits
+    def generate_full_personality(self):
+        personality = self.__generate_personality()
+        return self.__fill_up(personality)
+
+    # A method to generate a personality based on a new user
+    def remix_personality(self):
+        pass
 
     # A method to generate a random personality. It returns a list of personality trait strings.
-    def generate_personality(self):
+    def __generate_personality(self):
         encoded_personality = self.__generate_encoded_personality()
         return self.__decode_personality(encoded_personality)
 
@@ -34,3 +44,31 @@ class Bacon:
         decoded_personality_list = [i + "." for i in encoded_personality_string.split(".")]
         decoded_personality_list.pop()
         return decoded_personality_list
+
+    # A method that takes an existing personality and fills it up until it has the required amount of traits
+    def __fill_up(self, personality: list):
+        required = self.__required_amount
+        copy_personality = personality
+        amount = len(copy_personality)
+
+        # Loop until personality has the required amount or more
+        while amount < required:
+            new_personality = self.__generate_personality()
+            new_amount = len(new_personality)
+
+            # Determine if new personality has required amount
+            if new_amount == required:
+                copy_personality = new_personality
+                amount = new_amount
+            else:
+                copy_personality = copy_personality + new_personality
+                amount = len(copy_personality)
+
+        difference = amount - required
+
+        # Loop until personality is equal to required amount
+        for i in range(difference):
+            copy_personality.pop()
+            amount = amount - 1
+
+        return copy_personality
