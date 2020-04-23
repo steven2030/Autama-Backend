d249c1c6654509ca828867effd68e44a9a21adcd
MdoubleA <needanothermichael@gmail.com>
2020-04-22 19:51:19 -0700

    Create button in AutamaProfiles section of the admin site for mass producing Autama. Actual functionality of Autama mass production not added.

--- AutamaProfiles/admin.py
+++ AutamaProfiles/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import AutamaProfile
+from django.urls import path
+from django.http import HttpResponseRedirect
 
 # Register your models here.
-admin.site.register(AutamaProfile)
+@admin.register(AutamaProfile)
+class AutamaProfileAdmin(admin.ModelAdmin):
+    change_list_template = "AutamaProfiles/autama_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('massproduce/', self.massproduce),
+        ]
+        return my_urls + urls
+
+    def massproduce(self, request):
+        self.message_user(request, "Mass-production complete.")
+        return HttpResponseRedirect("../")
