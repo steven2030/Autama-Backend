f05a8ae0f0a8e96a96eb0ddd99540680f0c00510
timbercoder <jacklai@pdx.edu>
2020-04-27 11:20:37 -0700

    add information

--- language.py
+++ language.py
@@ -10,7 +10,9 @@ def main():
     print("Downloading and caching the pre-trained language could take 20 minutes or more.")
     bacon = Bacon()
     personality = bacon.generate_full_personality()
-    print("The process is done. Here is an example personality: ")
+    print("The process is done.")
+    print('There should now be a file named "dataset_cache_OpenAIGPTTokenizer" in the same directory the "manage.py" file is in.')
+    print("Here is an example personality:")
     print(personality)
 
 
