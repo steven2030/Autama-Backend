413333b6e0197f29b2117ec92b4f50ba0439e0e5
timbercoder <jacklai@pdx.edu>
2020-04-29 14:41:47 -0700

    change create new Autama method to a global function

--- AutamaProfiles/models.py
+++ AutamaProfiles/models.py
@@ -1,6 +1,33 @@
 from django.db import models
 from django.utils.text import slugify
 from django.shortcuts import reverse
+from Nucleus.pancake import Pancake
+
+
+# A function to create a new Autama profile. origin is the username of the user the Autama was based off
+def create_new_autama(personality: list, creator: str = "Happy Slackers", origin: str = "Happy Slackers"):
+    pancake = Pancake()  # For handling name generating
+    REQUIRED = 6  # The required amount of traits
+    amount = len(personality)
+
+    # Make sure personality has the required amount of traits
+    if amount == REQUIRED:
+        picture = "Images/a1.png"
+        first = pancake.generate_male_name()
+        last = "last name"
+        interest1 = personality[0]
+        interest2 = personality[1]
+        interest3 = personality[2]
+        interest4 = personality[3]
+        interest5 = personality[4]
+        interest6 = personality[5]
+
+        hybrid_autama = AutamaProfile.objects.create(creator=creator, picture=picture, first=first, last=last,
+                                                     pickle=origin,
+                                                     interest1=interest1, interest2=interest2,
+                                                     interest3=interest3, interest4=interest4,
+                                                     interest5=interest5, interest6=interest6)
+        hybrid_autama.save()
 
 
 class AutamaProfile(models.Model):
