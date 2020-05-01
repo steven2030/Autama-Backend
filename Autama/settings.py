422b39a3fb27d5392148b631cfbf36d98332cd0e
Ubuntu <ubuntu@ip-172-31-47-53.us-east-2.compute.internal>
2020-05-01 04:48:49 +0000

    adding files for serving publically

--- Autama/settings.py
+++ Autama/settings.py
@@ -22,9 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0jx6nply=59sv85ii0*r9abdg$sf+awn1vjwelfzc7pxfaek-3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.16.189.148']
+ALLOWED_HOSTS = ['3.16.189.148'] #, '172.31.47.53'] #server ip address
 
 
 # Application definition
