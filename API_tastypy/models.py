19b2ac2d71af74a1d81d2670f0527912b51075f8
MdoubleA <needanothermichael@gmail.com>
2020-05-17 21:27:20 -0700

    Add authorization to accounts end point, user can only see their own account information. Add authentication to accounts end point, client sends username and password with get request. Http basic authentication. Add ability to use Apikey, but will need to implement local storage in client.

--- API_tastypy/models.py
+++ API_tastypy/models.py
@@ -3,6 +3,7 @@ from tastypie.resources import ModelResource
 from AutamaProfiles.models import AutamaProfile
 from accounts.models import User
 from tastypie.authorization import Authorization
+from tastypie.authentication import ApiKeyAuthentication, BasicAuthentication
 from tastypie.models import ApiKey
 from django.urls import path
 from django.conf.urls import url
@@ -19,13 +20,24 @@ class AutamaResource(ModelResource):
                   'interest6']
 
 
+class AccountAuthorization(Authorization):
+    def read_list(self, object_list, bundle):
+        return object_list.filter(id=bundle.request.user.id)
+
+
 class AccountsResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'accounts'
-        fields = ['id', 'username', 'password', 'first_name', 'last_name', 'email', 'interest1', 'interest2', 'interest3',
-                  'interest4', 'interest5', 'interest6']
-        # authorization = Authorization()
+        fields = ['id', 'username']
+        filtering = {
+            'username': ['exact']
+        }
+
+        #authorization = Authorization()
+        authorization = AccountAuthorization()
+        #authentication = ApiKeyAuthentication()
+        authentication = BasicAuthentication()
 
 
 class RegistrationResource(ModelResource):
@@ -40,9 +52,16 @@ class RegistrationResource(ModelResource):
             user = User.objects.create(username=bundle.data.get('username'))
             user.set_password(bundle.data.get('password'))
             user.save()
+            apikey = bundle.data.get('apikey')
+            user = User.objects.get(username=bundle.data.get('username'))
+            apikey = ApiKey.objects.create(key=apikey, user=user)
+            apikey.save()
         except IntegrityError:
             raise BadRequest('That username already exists')
         return bundle
+
+
+
 
 
 '''
