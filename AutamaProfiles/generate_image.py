5be98c874f43b0607a8ef522dfbcc960fa127a6c
Nick McGettigan <Nick.McGettigan@gmail.com>
2020-05-18 15:29:35 -0700

    Update Find Matches algorithm to handle deleted autama.

--- AutamaProfiles/generate_image.py
+++ AutamaProfiles/generate_image.py
@@ -7,11 +7,6 @@ import time
 number_images = 5
 start_image = 152
 
-cmd = 'aws ssm send-command --instance-ids i-01d6de0c8312b250c --region us-west-2 --document-name "AWS-RunRemoteScript" --comment "Generate Images" '
-par = '--parameters commands=["runuser -l ubuntu -c \'python3.6 /home/ubuntu/stylegan/pretrained_example.py ' + str(number_images) + " " + str(start_image) + '\'"]'
-cmdpar = cmd+par
-print(cmdpar)
-
 tmp = subprocess.Popen('aws ec2 start-instances --instance-ids i-01d6de0c8312b250c', shell=True, stdout=subprocess.PIPE).communicate()
 time.sleep(120)
 
@@ -24,6 +19,6 @@ while (jstr['InstanceStatuses'][0]['InstanceState']['Name'] != "running"):
     time.sleep(30)
 
 # Generate images
-tmp = subprocess.Popen(cmdpar, shell=True, stdout=subprocess.PIPE).communicate()
+tmp = subprocess.Popen('aws ssm send-command --document-name "AWS-RunShellScript" --document-version "1" --targets \'[{"Key":"InstanceIds","Values":["i-01d6de0c8312b250c"]}]\' --parameters \'{"commands":["runuser -l ubuntu -c \'"\'"\'python3.6 /home/ubuntu/stylegan/pretrained_example.py 1 151\'"\'"\'"],"workingDirectory":[""],"executionTimeout":["3600"]}\' --timeout-seconds 600 --max-concurrency "50" --max-errors "0" --region us-west-2', shell=True, stdout=subprocess.PIPE).communicate()
     
 
