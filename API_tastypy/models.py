8e8850d20cc4593605713bd81732b8f85459a377
MdoubleA <needanothermichael@gmail.com>
2020-06-01 18:00:54 -0700

    Code clean.

--- API_tastypy/models.py
+++ API_tastypy/models.py
@@ -17,6 +17,7 @@ class UnmatchedAutamaAuthorization(Authorization):
 
         return unmatched_autama
 
+
 class UnmatchedAutamaResource(ModelResource):
     class Meta:
         queryset = AutamaProfile.objects.all()
