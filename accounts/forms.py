feeadc14fc19dc436cec47827ef4138139ea6193
MdoubleA <needanothermichael@gmail.com>
2020-06-04 18:56:53 -0700

    Code clean API_tastypy.py/views.py.

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,4 +18,4 @@ class EditProfileForm(UserChangeForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
-        # exclude = ('password',)
+
