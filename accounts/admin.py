93e3c2d25cdc526572a2c6ad6d6c3697bdb964fb
MdoubleA <needanothermichael@gmail.com>
2020-06-04 18:56:30 -0700

    Code clean API_tastypy.py/views.py.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import User, Messages, Matches, Claims
-from AutamaProfiles.models import AutamaProfile
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Messages)
