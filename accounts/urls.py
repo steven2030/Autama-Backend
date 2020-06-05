43e14cac763df562df06cc98d46fa2b8b95b4b7a
MdoubleA <needanothermichael@gmail.com>
2020-06-04 18:57:37 -0700

    Code clean API_tastypy.py/views.py.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,6 @@ app_name = 'accounts'  # For something akin to namespacing.
 
 urlpatterns = [
     path('register/', accounts_views.RegisterView.as_view(), name="register"),
-    #path('profile/edit', accounts_views.edit_profile, name='edit_user_profile'),
     path('profile/', accounts_views.ProfileView.as_view(), name='profile'),
     path('change-avatar/', ChangeAvatarView.as_view(), name='change-avatar'),
     path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
